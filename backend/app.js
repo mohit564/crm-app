bf717ad8b1ccb52900b6e4cf87be118bc7cf0120
mohit564 <86644125+mohit564@users.noreply.github.com>
2021-09-09 17:10:16 +0530

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+﻿const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to CRM App!");
+});
+
+app.listen(PORT, () => {
+  console.log("listening on port " + PORT);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,8 @@
+﻿import React from "react";
+import "./App.css";
+
+const App = () => {
+  return <h1>Hello World</h1>;
+};
+
+export default App;
