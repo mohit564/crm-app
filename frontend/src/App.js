85228f831e037be3363f33bedb0178f84975fcd7
Mohit Dhule <86644125+mohit564@users.noreply.github.com>
2021-10-22 17:42:39 +0530

    feat: add lazy loading for performance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-﻿import React from "react";
+﻿import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
@@ -6,20 +6,28 @@ import "./App.css";
 import HomePage from "./components/HomePage";
 import Login from "./components/Authentication/Login";
 import ForgetPassword from "./components/Authentication/ForgetPassword";
-import Dashboard from "./components/Dashboard/Dashboard";
 import Register from "./components/Authentication/Register";
-import Users from "./components/Users/Users";
-import Leads from "./components/Leads/Leads";
-import CreateLead from "./components/Leads/CreateLead";
-import EditLead from "./components/Leads/EditLead";
-import Contacts from "./components/Contacts/Contacts";
-import CreateContact from "./components/Contacts/CreateContact";
-import EditContact from "./components/Contacts/EditContact";
-import ServiceRequests from "./components/ServiceRequests/ServiceRequests";
-import CreateServiceRequest from "./components/ServiceRequests/CreateServiceRequest";
-import EditServiceRequest from "./components/ServiceRequests/EditServiceRequest";
 import ProtectedRoute from "./components/ProtectedRoute";
 import NotFound from "./components/NotFound/NotFound";
+import Spinner from "./components/Spinner/Spinner";
+
+const Dashboard = lazy(() => import("./components/Dashboard/Dashboard"));
+const Users = lazy(() => import("./components/Users/Users"));
+const Leads = lazy(() => import("./components/Leads/Leads"));
+const CreateLead = lazy(() => import("./components/Leads/CreateLead"));
+const EditLead = lazy(() => import("./components/Leads/EditLead"));
+const Contacts = lazy(() => import("./components/Contacts/Contacts"));
+const CreateContact = lazy(() => import("./components/Contacts/CreateContact"));
+const EditContact = lazy(() => import("./components/Contacts/EditContact"));
+const ServiceRequests = lazy(() =>
+  import("./components/ServiceRequests/ServiceRequests")
+);
+const CreateServiceRequest = lazy(() =>
+  import("./components/ServiceRequests/CreateServiceRequest")
+);
+const EditServiceRequest = lazy(() =>
+  import("./components/ServiceRequests/EditServiceRequest")
+);
 
 const App = (props) => {
   let token = localStorage.getItem("token");
@@ -28,43 +36,73 @@ const App = (props) => {
     <Router>
       <Switch>
         <Route path="/" exact>
-          {token ? <Dashboard /> : <HomePage />}
+          {!token ? (
+            <HomePage />
+          ) : (
+            <Suspense fallback={Spinner}>
+              <Dashboard />
+            </Suspense>
+          )}
         </Route>
         <Route path="/login" component={Login} exact />
         <Route path="/register" component={Register} exact />
         <Route path="/forget-password" component={ForgetPassword} exact />
 
-        <ProtectedRoute path="/dashboard" component={Dashboard} exact />
-        <ProtectedRoute path="/users" component={Users} exact />
-        <ProtectedRoute path="/leads" component={Leads} exact />
-        <ProtectedRoute path="/leads/create" component={CreateLead} exact />
-        <ProtectedRoute path="/leads/edit/:id" component={EditLead} exact />
-        <ProtectedRoute path="/contacts" component={Contacts} exact />
-        <ProtectedRoute
-          path="/contacts/create"
-          component={CreateContact}
-          exact
-        />
-        <ProtectedRoute
-          path="/contacts/edit/:id"
-          component={EditContact}
-          exact
-        />
-        <ProtectedRoute
-          path="/service-requests"
-          component={ServiceRequests}
-          exact
-        />
-        <ProtectedRoute
-          path="/service-requests/create"
-          component={CreateServiceRequest}
-          exact
-        />
-        <ProtectedRoute
-          path="/service-requests/edit/:id"
-          component={EditServiceRequest}
-          exact
-        />
+        <ProtectedRoute path="/dashboard" exact>
+          <Suspense fallback={Spinner}>
+            <Dashboard />
+          </Suspense>
+        </ProtectedRoute>
+        <ProtectedRoute path="/users" exact>
+          <Suspense fallback={Spinner}>
+            <Users />
+          </Suspense>
+        </ProtectedRoute>
+        <ProtectedRoute path="/leads" exact>
+          <Suspense fallback={Spinner}>
+            <Leads />
+          </Suspense>
+        </ProtectedRoute>
+        <ProtectedRoute path="/leads/create" exact>
+          <Suspense fallback={Spinner}>
+            <CreateLead />
+          </Suspense>
+        </ProtectedRoute>
+        <ProtectedRoute path="/leads/edit/:id" exact>
+          <Suspense fallback={Spinner}>
+            <EditLead />
+          </Suspense>
+        </ProtectedRoute>
+        <ProtectedRoute path="/contacts" exact>
+          <Suspense fallback={Spinner}>
+            <Contacts />
+          </Suspense>
+        </ProtectedRoute>
+        <ProtectedRoute path="/contacts/create" exact>
+          <Suspense fallback={Spinner}>
+            <CreateContact />
+          </Suspense>
+        </ProtectedRoute>
+        <ProtectedRoute path="/contacts/edit/:id" exact>
+          <Suspense fallback={Spinner}>
+            <EditContact />
+          </Suspense>
+        </ProtectedRoute>
+        <ProtectedRoute path="/service-requests" exact>
+          <Suspense fallback={Spinner}>
+            <ServiceRequests />
+          </Suspense>
+        </ProtectedRoute>
+        <ProtectedRoute path="/service-requests/create" exact>
+          <Suspense fallback={Spinner}>
+            <CreateServiceRequest />
+          </Suspense>
+        </ProtectedRoute>
+        <ProtectedRoute path="/service-requests/edit/:id" exact>
+          <Suspense fallback={Spinner}>
+            <EditServiceRequest />
+          </Suspense>
+        </ProtectedRoute>
         <Route path="*" component={NotFound} />
       </Switch>
     </Router>
